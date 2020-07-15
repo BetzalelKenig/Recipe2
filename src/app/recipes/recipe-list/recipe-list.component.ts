fab4a0cb7abdf0e732c0fdff8d331dc6d1adb136
BetzalelKenig <59796236+BetzalelKenig@users.noreply.github.com>
2020-07-15 17:24:00 +0300

    finish 86

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipe = [];
+  recipes: Recipe[] = [
+    new Recipe('Test Recipe', 'french crepe', 'https://p0.pikist.com/photos/832/1006/pancake-crepes-eat-food-crepe-thumbnail.jpg'),
+    new Recipe('Test Recipe', 'french crepe', 'https://p0.pikist.com/photos/832/1006/pancake-crepes-eat-food-crepe-thumbnail.jpg'),
+  ];
 
   constructor() { }
 
