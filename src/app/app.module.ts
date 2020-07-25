aa5d9c5b6e193326403f19366c6a0e0555442f73
Betzalel Kenig <betzalelkenig@gmail.com>
2020-07-25 22:27:13 +0300

    finish section 22

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,36 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { AppRoutingModule } from './app-routing.module';
-import { AuthComponent } from './auth/auth.component';
 import { AuthIntersetorService } from './auth/auth-intersetor.service';
-import { RecipesModule } from './recipes/recipes.module';
-import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { SharedModule } from './shared/shared.module';
 
+
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, AuthComponent],
+  declarations: [AppComponent, HeaderComponent],
   imports: [
     BrowserModule,
-    FormsModule,
-    ReactiveFormsModule,
     HttpClientModule,
     AppRoutingModule,
-    RecipesModule,
-    ShoppingListModule,
     SharedModule,
   ],
   providers: [
     {
+      /* provide the interseter for add token to http requests */
       provide: HTTP_INTERCEPTORS,
       useClass: AuthIntersetorService,
       multi: true,
     },
-  ] /* provide the interseter for add token to http requests */,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
